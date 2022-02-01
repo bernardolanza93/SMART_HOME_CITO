e767f3b78d2443de29bad978b0d8dc4765de2007
Bernardo Lanza <87471233+bernardolanza93@users.noreply.github.com>
2022-02-01 18:45:44 +0100

    Add files via upload

--- exic_code.py
+++ exic_code.py
@@ -4,38 +4,105 @@ import telepot
 from telepot.namedtuple import ReplyKeyboardMarkup, InlineKeyboardButton
 from telepot.namedtuple import InlineKeyboardMarkup, KeyboardButton
 from telepot.loop import MessageLoop
+from gpiozero import CPUTemperature
+import time
 from time import sleep
-import datetime
-import paho.mqtt.client as mqtt
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+import multiprocessing
+import os
+import cv2
+
+import reciver
+
+import csv
+import pandas as pd
+
+
+string_from_tcp_ID = "null"
+
 
 
 # Our "on message" event
+def make_graph():
 
-def on_message(client, userdata, message):
-    topic = str(message.topic)
-    message = str(message.payload.decode("utf-8"))
-    print(topic + message)
+    headers = ['data','temp','hum']
+    df = pd.read_csv('temp_dataRPCITO.csv',names=headers)
+    print (df)
 
-def client_send_datatopic_tobroker(data, topic,broker):
-    print("creating new instance")
-    client = mqtt.Client("client_citofono") #create new instance
-    #client.on_message = on_message
-    print("connecting to broker")
-    client.connect(broker_address_mansarda,1883) #connect to broker
-
-    print("Subscribing to topic",topic_citofono)
-    client.subscribe(topic_citofono)
-    print("Publishing message to topic",topic_citofono)
-    client.publish(topic_citofono ,data_citofono)#publish
+    df['data'] = df['data'].map(lambda x: datetime.strptime(str(x), '%Y-%m-%d %H:%M:%S'))
+    x = df['data']
+    y = df['temp']
+    z = df['hum']
 
 
+    fig, ax1 = plt.subplots()
 
+    color = 'tab:red'
+    ax1.set_xlabel('time')
+    ax1.set_ylabel('Temp', color=color)
+    ax1.plot(x, y, color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+    color = 'tab:blue'
+    ax2.set_ylabel('hum', color=color)  # we already handled the x-label with ax1
+    ax2.plot(x,z, color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+
+    plt.savefig('plot_data_citofono.png')
+
+    #The simplest way is to start the ntetwork loop on a separate thread using the client.loop_start() function, then use the normal client.publish method
+
+
+def write_data_csv():
+    
+    while True:
+        hum = float(0)
+        temp = float(0)
+        hum, temp = Adafruit_DHT.read_retry(sensor, pin)
+        print("read : ",hum,temp)
+        now = datetime.now()
+        current_time = now.strftime('%Y-%m-%d %H:%M:%S')
+        
+           
+        #print("data",t,C,H)
+        
+        f = open('temp_dataRPCITO.csv', 'a')
+        writer = csv.writer(f)
+        print("read complete : ",current_time,temp,hum)
+        writer.writerow([current_time,temp,hum])
+        make_graph()
+        time.sleep(60)
+        
+        
+        
+
+
+
+
+
+
+
+#plt.show()
+
+
+
+# when connecting to mqtt do this;
+# receive messages on this topic
+
+    
 def on_chat_message(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
                                     [InlineKeyboardButton(text="INFO_CASA",callback_data='info'),
                                      InlineKeyboardButton(text="CANCELLO",callback_data='open1'),
-                                     InlineKeyboardButton(text="CANC_ETTO",callback_data='open2')
+                                     InlineKeyboardButton(text="REBOOT",callback_data='open2')
                                      ]
                                     
                                 ]
@@ -48,13 +115,18 @@ def on_chat_message(msg):
 def on_callback_query(msg):
     query_id, chat_id, query_data = telepot.glance(msg, flavor='callback_query')
     print('Callback Query:', query_id, chat_id, query_data)
+    file_path = '/home/pi/plot_data_citofono.png'
+    file_path1 = '/home/pi/plot_rec.png'
     if query_data=='info':
-        now = datetime.datetime.now()
+        now = datetime.now()
         hourstr = now.strftime("%Y-%m-%d %H:%M:%S")
-        hum, temp = Adafruit_DHT.read_retry(sensor, pin)
-        bot.sendMessage(chat_id, 'temperature: %s'%temp)
-        bot.sendMessage(chat_id, 'humidity: %s'%hum)
+        cpu = CPUTemperature()
+        bot.sendMessage(chat_id, 'CPU_temp citofono: %s'%str(cpu.temperature))
         bot.sendMessage(chat_id, str(hourstr))
+        bot.sendMessage(chat_id, 'temp citofono: ')
+        bot.sendPhoto(chat_id, open(file_path, 'rb'))
+        bot.sendMessage(chat_id, 'temp mansarda: ')
+        bot.sendPhoto(chat_id, open(file_path1, 'rb'))
         print("bot COM succesful")
 
     elif query_data=='open1':
@@ -73,30 +145,55 @@ def on_callback_query(msg):
         
     elif query_data=='open2':
         print("open2")
-        bot.sendMessage(chat_id, "apertura cancelletto...")
+        bot.sendMessage(chat_id, "reboot in corso...")
+        os.system('sudo reboot')
+        
 
 
+def bot_ini(bot):
+    
+    MessageLoop(bot, {'chat': on_chat_message,
+      'callback_query': on_callback_query}).run_as_thread()
+    print("listaning... bot online")
+    while True:
+        sleep(1)
+        now = datetime.now()
+        print(now.strftime("%Y-%m-%d %H:%M:%S"))
+    
 
-data_citofono = "init"
-broker_address_mansarda="192.168.1.90"
-topic_citofono = "house/data_citofono"
 
 sensor = Adafruit_DHT.DHT11
 pin = 4
 
 
-bot = telepot.Bot('2070265556:AAHtStxZRT_J9hxvBtC7EKdnfM6sXVOgJ4U')
+
+
 humidity = 0.0
 temperature = 0.0
+bot = telepot.Bot('2070265556:AAHtStxZRT_J9hxvBtC7EKdnfM6sXVOgJ4U')
 
-MessageLoop(bot, {'chat': on_chat_message,
-      'callback_query': on_callback_query}).run_as_thread()
-print("listaning..")
-while True:
-    sleep(5)
-    now = datetime.datetime.now()
-    print(now.strftime("%Y-%m-%d %H:%M:%S"))
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    #client_send_datatopic_tobroker(data_citofono,topic_citofono,broker_address_mansarda)        
-    
 
+
+
+
+print("inizializing process 1")
+
+p1 = multiprocessing.Process(target=reciver.listen_for_TCP_string, args=(string_from_tcp_ID,))
+p2 = multiprocessing.Process(target=bot_ini,args = (bot,))
+p3_ri = multiprocessing.Process(target=reciver.main)
+p_sensor = multiprocessing.Process(target=write_data_csv)
+p1.start()
+p2.start()
+p3_ri.start()
+p_sensor.start()
+
+
+print("ID of process p1: {}".format(p1.pid))
+print("ID of process p1: {}".format(p2.pid))
+print("ID of process p1: {}".format(p3_ri.pid))
+print("ID of process p1: {}".format(p_sensor))
+
+p1.join()
+p2.join()
+p3_ri.join()
+p_sensor.join()
