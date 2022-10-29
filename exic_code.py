d75f949db6d61d9648a90558dde05dd42a5fe8d9
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2022-10-29 21:18:38 +0200

    added reciver and logging

--- exic_code.py
+++ exic_code.py
@@ -160,7 +160,7 @@ def on_callback_query(msg):
         cpu = CPUTemperature()
         bot.sendMessage(chat_id, 'CPU_temp citofono: %s'%str(cpu.temperature))
         bot.sendMessage(chat_id, str(hourstr))
-        bot.sendDocument(id, open(path + "RPI_SH.log", 'rb'))
+        bot.sendDocument(chat_id, open(path + "RPI_SH.log", 'rb'))
         print("bot COM succesful")
 
     elif query_data=='open1':
