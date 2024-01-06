16ca2783038db9f1e82048abb49baf821cb7d40d
bernardolanza93 <bernardo.lanza.tech@gmail.com>
2024-01-06 14:47:37 +0100

    update error gen 2024

--- exicator.sh
+++ exicator.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 cd
+cd  SMART_HOME_CITO/
+
+echo "CHECKK INTERNET CONNECTION... "
+wget -q --spider http://google.com
+
+if [ $? -eq 0 ]; then
+    echo "ONLINE"
+else
+    echo "OFFLINE"
+fi
+
+
+git pull
+
+echo  "SMART CITO UPDATED SUCCESFULLY"
+echo "STARTING CITO SYSTEM "
+
+
+
+echo ""
+
+echo "       _                 _         "
+echo " _   _| |__  _   _ _ __ | |_ _   _ "
+echo "| | | | '_ \| | | | '_ \| __| | | |"
+echo "| |_| | |_) | |_| | | | | |_| |_| |"
+echo " \__,_|_.__/ \__,_|_| |_|\__|\__,_|"
+
+echo ""
+
+
 python3 exic_code.py
